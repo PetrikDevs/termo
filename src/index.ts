5380c30aa12e86018641c743d991ef2dcb09e3ba
Preisler25 <preisler.andras@gmail.com>
2023-11-22 18:44:05 +0100

    cors added

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,19 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import { json } from 'express';
 import testsRoutes from './router/testsRoutes';
-
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }
+));
 app.use(json());
 
 app.use(testsRoutes);
