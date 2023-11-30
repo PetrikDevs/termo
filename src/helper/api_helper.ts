21645753935c9649fb7f2a62296d821cd5ae280f
Preisler25 <preisler.andras@gmail.com>
2023-11-30 15:57:13 +0100

    szelep

--- src/helper/api_helper.ts
+++ src/helper/api_helper.ts
@@ -3,8 +3,18 @@ import { Motors } from "../model/motors";
 
 const getMotors = async () => {
     try {
+        const motors: Motors = {
+            mot0: false,
+            mot1: false,
+            mot2: false,
+            mot3: false,
+            mot4: false
+        }
+        return motors;
+        /*
         const res = await axiosConfig.get('/motors');
         return res.data.data;
+        */
     } catch (error) {
         console.error(error);
     }
