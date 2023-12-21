e6c0b3ecbfb869cfabbdf4c81ba2e27ea6afa23f
Preisler25 <preisler.andras@gmail.com>
2023-12-21 15:45:32 +0100

    test

--- src/controller/valveController.ts
+++ src/controller/valveController.ts
@@ -22,7 +22,7 @@ export default class ValveController {
 
     public setValve(req: Request, res: Response) {
         try {
-            console.log(req.body.valve);
+            console.log(req.body);
             let valve: Valve =  this.valveService.formatValve(req.body.valve);
             let result = this.valveService.setValve(valve);
             res.json({ data: result });
