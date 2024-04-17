df6b64727ba290ea622d362acbb66ccd12a35a07
Preisler25 <preisler.andras@gmail.com>
2024-04-17 11:37:05 +0200

    test: ?

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,32 +1,32 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class ApiService {
-    private readonly base: any;
-    constructor() {
-        this.base = axios.create({
-            baseURL: process.env.API_URL,
-            timeout: 10000,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-    }
-    
-    public async get(path: string) {
-        try {
-            const res = await this.base.get(path);
-            return res;
-        } catch (error) {
-            console.error(error);
-        }
-    }
+  private readonly base: any;
+  constructor() {
+    this.base = axios.create({
+      baseURL: process.env.API_URL,
+      timeout: 10000,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-    public async post(path: string, data: any) {
-        try {
-            const res = await this.base.post(path, { data });
-            return res;
-        } catch (error) {
-            console.error(error);
-        }
+  public async get(path: string) {
+    try {
+      const res = await this.base.get(path);
+      return res;
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  public async post(path: string, data: any) {
+    try {
+      const res = await this.base.post(path, data);
+      return res;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
